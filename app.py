08baa3d8d9b033c54ce4978bfb25e59e1d9a48dd
Isara Naranirattisai <isaradream@gmail.com>
2017-02-02 22:01:59 +0700

    Training 'part 1'

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 ##############################################################
 #         Basic Flask Course @tedESL ESL, KMITL              #
-#              Github:https://github.com/DreamN/Basic-Flask  #	
+#              Github:https://github.com/DreamN/Basic-Flask  #
 ##############################################################
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def Home():
-	return "Hello World"
+	y = "foo"
+	return render_template('home.html', x=y)
+
+@app.route('/game')
+def myFavGame():
+	game_list = ['Mario', 'Pokemon', 'Pacman', 'Rockman', 'Megaman']
+	return render_template('favGame.html', game_list=game_list)
+
+@app.route('/greeting/<string:name>/')
+def greeting(name):
+	name = name[::-1]
+	return render_template('greeting.html', name=name)
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=80)
